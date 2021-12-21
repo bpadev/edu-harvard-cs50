e47c82e52dfa2b8085089473fb0a16efa22b70e6
Benjamin Arsenault <hi@benjamin.dev>
2021-12-20 20:17:53 -0500

    Add pointer notes

---
+++ 04-memory/lecture.c
@@ -0,0 +1,15 @@
+// Week 4, Memory
+
+
+// Hexadecimal
+// - Base 16, 0-9, A-F, so 0-F. 
+// - 1 Hexadecimal digit = 4 binary digits/bits
+// - 0x prefix, 0x48
+
+
+// Addresses
+// - 
+& // - address of operator
+* // - dereference operator, says go to this address
+// - pointers are just a different type of variable that store the address of some value.
+// - 
