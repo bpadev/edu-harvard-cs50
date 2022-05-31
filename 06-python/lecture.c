bf749bc66aff8ef82b17b907155f20bb793d7c0d
Benjamin Arsenault <hi@benjamin.dev>
2022-05-31 13:30:23 -0400

    Add base notes for python lecture

---
+++ 06-python/lecture.c
@@ -0,0 +1,12 @@
+// Week 6, Python
+
+
+// Python vs C
+printf("Hello, world!\n");
+print("Hello, world")
+
+
+// Python CS50
+import cs50
+// Python is interpreted, not compiled.
+python filename.py
